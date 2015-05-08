6e53d42a8b29e4aa27a3c54944d10f8a6fae0273
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-08 14:23:02 +0200

    ajout readFile

--- rasp-temp/src/main/java/fr/aleclerc/rasp/temp/infra/service/CapteurService.java
+++ rasp-temp/src/main/java/fr/aleclerc/rasp/temp/infra/service/CapteurService.java
@@ -1,5 +1,7 @@
 package fr.aleclerc.rasp.temp.infra.service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystems;
@@ -8,8 +10,6 @@ import java.nio.file.Path;
 
 public class CapteurService {
 
-	
-	
 	public CapteurService() {
 		super();
 
@@ -21,64 +21,83 @@ public class CapteurService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		getDeviceFile();
+		Path path = getDeviceFile();
+		readFile( path);
 	}
-	
-	
-	private String getDeviceFile(){
+
+	private Path getDeviceFile() {
+		Path resultat = null;
 		String baseDir = "/sys/bus/w1/devices/";
-//		String deviceFolder = baseDir+"28*";
-//		String devideFile = deviceFolder+"/w1_slave";
-//		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + "28*");
+		DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+			@Override
+			public boolean accept(Path file) throws IOException {
 
-	
-	   DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
-	        @Override
-	        public boolean accept(Path file) throws IOException {
-	        	
-	        	System.out.println(file.getFileName());
-	        	System.out.println(Files.isDirectory(file));
-	            return ((Files.isDirectory(file))&&file.getFileName().toString().startsWith("28"));
-	        }
-	    };
+				return ((Files.isDirectory(file)) && file.getFileName()
+						.toString().startsWith("28"));
+			}
+		};
 
-	    Path dir = FileSystems.getDefault().getPath(baseDir);
-	    try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir,
-	            filter)) {
-	        for (Path path : stream) {
-	            // Iterate over the paths in the directory and print filenames
-	            System.out.println("yes"+path.getFileName());
-	        }
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	    return null;
+		Path dir = FileSystems.getDefault().getPath(baseDir);
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir,
+				filter)) {
+			for (Path path : stream) {
+				resultat = path;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return resultat;
 	}
-//	    
-//	base_dir = '/sys/bus/w1/devices/'
-//			device_folder = glob.glob(base_dir + '28*')[0]
-//			device_file = device_folder + '/w1_slave'
-//
-//			def read_temp_raw():
-//			 f = open(device_file, 'r')
-//			 lines = f.readlines()
-//			 f.close()
-//			 return lines
-//
-//			def read_temp():
-//			 lines = read_temp_raw()
-//			 while lines[0].strip()[-3:] != 'YES':
-//			   time.sleep(0.2)
-//			   lines = read_temp_raw()
-//			 equals_pos = lines[1].find('t=')
-//			 if equals_pos != -1:
-//			   temp_string = lines[1][equals_pos+2:]
-//			   temp_c = float(temp_string) / 1000.0
-//			   temp_f = temp_c * 9.0 / 5.0 + 32.0
-//			   return temp_c, temp_f
-//
-//			while True:
-//			 print(read_temp())
-//			 time.sleep(1)
+	private String readFile(Path path){
+		BufferedReader br = null;
+		 StringBuilder sb = new StringBuilder();
+		try {
+ 
+			String sCurrentLine;
+ 
+			br = new BufferedReader(new FileReader(path.toFile()));
+ 
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+				sb.append(sCurrentLine);
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+		return sb.toString();
+		
+	}
+	// base_dir = '/sys/bus/w1/devices/'
+	// device_folder = glob.glob(base_dir + '28*')[0]
+	// device_file = device_folder + '/w1_slave'
+	//
+	// def read_temp_raw():
+	// f = open(device_file, 'r')
+	// lines = f.readlines()
+	// f.close()
+	// return lines
+	//
+	// def read_temp():
+	// lines = read_temp_raw()
+	// while lines[0].strip()[-3:] != 'YES':
+	// time.sleep(0.2)
+	// lines = read_temp_raw()
+	// equals_pos = lines[1].find('t=')
+	// if equals_pos != -1:
+	// temp_string = lines[1][equals_pos+2:]
+	// temp_c = float(temp_string) / 1000.0
+	// temp_f = temp_c * 9.0 / 5.0 + 32.0
+	// return temp_c, temp_f
+	//
+	// while True:
+	// print(read_temp())
+	// time.sleep(1)
 }
-
